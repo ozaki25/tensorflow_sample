3895a2aa9c465a6faea857559f4fe912fa0f785a
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-10-29 16:28:29 +0900

    add yolo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Hello from './components/Hello';
 
 const App = () => (
   <div>
     <h1>Hello</h1>
-    <Hello />
   </div>
 );
 
--- src/utils/hello.js
+++ src/utils/hello.js
@@ -1,24 +1,71 @@
 import * as tf from '@tensorflow/tfjs';
+import yolo, { downloadModel } from 'tfjs-yolo-tiny';
+import { Webcam } from '../libs/webcam';
 
-async function hello() {
-  // Create a simple model.
-  const model = tf.sequential();
-  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+let model;
+const webcam = new Webcam(document.getElementById('webcam'));
+const webcamElem = document.getElementById('webcam-wrapper');
 
-  // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({
-    loss: 'meanSquaredError',
-    optimizer: 'sgd',
-  });
+(async function main() {
+  try {
+    model = await downloadModel();
+    alert('モデルの読み込みが完了しました');
+    await webcam.setup();
+    doneLoading();
+    run();
+  } catch (e) {
+    console.error(e);
+    showError();
+  }
+})();
 
-  // Generate some synthetic data for training. (y = 2x - 1)
-  const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-  const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
-
-  // Train the model using the data.
-  await model.fit(xs, ys, { epochs: 250 });
-
-  return model.predict(tf.tensor2d([20], [1, 1]));
+async function run() {
+  setInterval(async () => {
+    clearRects();
+    const inputImage = webcam.capture();
+    const t0 = performance.now();
+    const boxes = await yolo(inputImage, model);
+    const t1 = performance.now();
+    console.log(`YOLO inference took ${t1 - t0} milliseconds.`);
+    boxes.forEach(box => {
+      const { top, left, bottom, right, classProb, className } = box;
+      drawRect(
+        left,
+        top,
+        right - left,
+        bottom - top,
+        `${className}: ${Math.round(classProb * 100)}%`,
+      );
+    });
+    await tf.nextFrame();
+  }, 1000);
 }
 
-export default hello;
+function drawRect(x, y, w, h, text = '', color = 'red') {
+  console.log('rect', { x, y, w, h, text, color });
+  const rect = document.createElement('div');
+  const label = document.createElement('div');
+  rect.classList.add('rect');
+  rect.style.cssText = `top: ${y}; left: ${x}; width: ${w}; height: ${h}; border-color: ${color}`;
+  label.classList.add('label');
+  label.innerText = text;
+  rect.appendChild(label);
+  webcamElem.appendChild(rect);
+}
+
+function clearRects() {
+  const rects = document.getElementsByClassName('rect');
+  while (rects[0]) {
+    rects[0].parentNode.removeChild(rects[0]);
+  }
+}
+
+function doneLoading() {
+  const webcam = document.getElementById('webcam-wrapper');
+  webcam.style.display = 'flex';
+}
+
+function showError() {
+  alert('loading error');
+  doneLoading();
+}
